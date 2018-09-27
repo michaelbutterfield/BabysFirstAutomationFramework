f81d5336b87da848ed45399f7c55fb766c78c7e8
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-09-27 15:42:47 +0100

    craete -> create

--- training.automation.api/src/test/java/stepdefinitions/TrelloSteps.java
+++ training.automation.api/src/test/java/stepdefinitions/TrelloSteps.java
@@ -8,7 +8,7 @@ public class TrelloSteps
 	@Given ("^the API call response is correct$")
 	public static void theApiCallResponseIsCorrect()
 	{
-		//ApiHelper.createTrelloBoard("my Api Test Board", "Created using the Trello API calls");
+		ApiHelper.createTrelloBoard("my Api Test Board", "Created using the Trello API calls");
 				
 		ApiHelper.deleteTrelloBoard(ApiHelper.getTrelloBoardId("my Api Test Board"));
 	}
