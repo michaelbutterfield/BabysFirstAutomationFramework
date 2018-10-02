ce5da625989978fa87eb21e251d4ffab07cb3760
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-02 08:28:56 +0100

    Housekeeping - Sorting variables and functions A-Z

--- training.automation.selenium/src/main/java/application/pages/BoardsPage.java
+++ training.automation.selenium/src/main/java/application/pages/BoardsPage.java
@@ -8,16 +8,17 @@ import application.pages.elements.selenium.Label;
 
 public class BoardsPage extends Page
 {
-	public Button boardsSideBarButton;
-	public Button addButton;
-	public Button createNewBoardButton;
-	public InputBox nameInput;
-	public Button backgroundSelectionButton;
-	public Button createBoardButton;
-	public Button userBoardButton;
-	public Label boardNotFound;
-	public Button favouriteButton;
 	
+	public Button addButton;
+	public Button backgroundSelectionButton;
+	public Label boardNotFound;
+	public Button boardsSideBarButton;
+	public Button createBoardButton;
+	public Button createNewBoardButton;
+	public Button favouriteButton;
+	public InputBox nameInput;
+	public Button userBoardButton;
+		
 	public BoardsPage()
 	{
 		super("BoardsPage");
@@ -26,24 +27,28 @@ public class BoardsPage extends Page
 	
 	private void buildPage()
 	{
-		boardsSideBarButton = new Button(By.xpath("//*[@id=\"content\"]/div/div[2]/div/div/div/div/div/div[1]/nav/div[1]/ul/li[1]/a/span[2]"),
-										 "Boards Side Bar Button", name);
-		
 		addButton = new Button(By.xpath("//*[@id=\"header\"]/div[5]/a[1]/span"), "Add Button", name);
 		
-		createNewBoardButton = new Button(By.xpath("//*[@id=\"classic\"]/div[5]/div/div[2]/div/div/div/ul/li[1]/a"), "Create Board... Button", name);
-		
-		nameInput = new InputBox(By.xpath("//*[@id=\"classic\"]/div[4]/div/div/div/form/div/div/div[1]/input"), "Board Name Input Box", name);
-		
 		backgroundSelectionButton = new Button(By.xpath("//*[@id=\"classic\"]/div[4]/div/div/div/form/div/ul/li[2]/button"),
-											   "Board Background Selection", name);
-		
-		createBoardButton = new Button(By.xpath("//*[@id=\"classic\"]/div[4]/div/div/div/form/button/span[2]"), "Create Board Button", name);
-		
-		userBoardButton = new Button(By.xpath("//div[contains(@title, 'TestBoard')]"), "User Created Board", name);
+				   "Board Background Selection", name);
 		
 		boardNotFound = new Label(By.xpath("//h1[contains(text(),'Board not found.')]"), "Board not found. message", name);
 		
+		boardsSideBarButton = new Button(By.xpath("//*[@id=\"content\"]/div/div[2]/div/div/div/div/div/div[1]/nav/div[1]/ul/li[1]/a/span[2]"),
+										 "Boards Side Bar Button", name);
+		
+		createBoardButton = new Button(By.xpath("//*[@id=\"classic\"]/div[4]/div/div/div/form/button/span[2]"), "Create Board Button", name);
+		
+		createNewBoardButton = new Button(By.xpath("//*[@id=\"classic\"]/div[5]/div/div[2]/div/div/div/ul/li[1]/a"), "Create Board... Button", name);
+		
 		favouriteButton = new Button(By.xpath("//span[contains(@class, 'board-tile-options')]"), "Favourite Button", name);
+		
+		nameInput = new InputBox(By.xpath("//*[@id=\"classic\"]/div[4]/div/div/div/form/div/div/div[1]/input"), "Board Name Input Box", name);
+		
+		userBoardButton = new Button(By.xpath("//div[contains(@title, 'TestBoard')]"), "User Created Board", name);
+		
+		
+		
+		
 	}
 }
