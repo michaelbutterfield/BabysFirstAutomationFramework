e64f0bb7d19e48e9af94ac579ace275dec3cf609
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-02 08:31:31 +0100

    Helper class used in set up and tear down
    Log In and create board used in set up to consolidate list of function
    calls into one
    
    Delete board used in the tear down - might need tweaking for it to work
    every time in any situation

---
+++ training.automation.selenium/src/main/java/utilities/Helper.java
@@ -0,0 +1,70 @@
+package utilities;
+
+import org.junit.Assert;
+
+import application.DesktopWebsite;
+
+public class Helper
+{
+	public static void LogIn(String email, String password)
+	{
+		SeleniumDriverHelper.getWebDriver().get("http://www.trello.com/login");
+		
+		DesktopWebsite.logInPage.createAnAccount.assertElementIsDisplayed();
+		
+		DesktopWebsite.logInPage.emailAddress.inputText(email);
+		
+		DesktopWebsite.logInPage.password.inputText(password);
+		
+		DesktopWebsite.logInPage.logInButton.click();
+		
+		DesktopWebsite.boardsPage.addButton.waitForElementToBeClickable();
+		
+		Assert.assertEquals(SeleniumDriverHelper.getWebDriver().getTitle(), "Boards | Trello");
+	}
+	
+	public static void CreateBoard(String boardName)
+	{
+		DesktopWebsite.boardsPage.addButton.click();
+		
+		DesktopWebsite.boardsPage.createNewBoardButton.click();
+		
+		DesktopWebsite.boardsPage.nameInput.inputText(boardName);
+		
+		DesktopWebsite.boardsPage.backgroundSelectionButton.click();
+		
+		DesktopWebsite.boardsPage.createBoardButton.click();
+		
+		try
+		{
+			Thread.sleep(2000);
+		}
+		catch (Exception e)
+		{
+			
+		}
+		
+		DesktopWebsite.header.backToHome.click();
+		
+		DesktopWebsite.boardsPage.userBoardButton.assertElementIsDisplayed();
+	}
+	
+	public static void DeleteBoard()
+	{
+		DesktopWebsite.boardsPage.userBoardButton.click();
+		
+		DesktopWebsite.specificBoardsPage.moreSideMenuButton.click();
+		
+		DesktopWebsite.specificBoardsPage.closeBoard.click();
+		
+		DesktopWebsite.specificBoardsPage.closeBoardConfirmation.click();
+		
+		DesktopWebsite.specificBoardsPage.permDeleteBoard.click();
+		
+		DesktopWebsite.specificBoardsPage.permDeleteBoardConfirm.click();
+		
+		DesktopWebsite.boardsPage.boardNotFound.assertElementTextContains("Board not found.");
+		
+		DesktopWebsite.header.trelloLogoHome.click();
+	}
+}
