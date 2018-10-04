56edb64b478d7db9e75443d3bda0aa03bbd5f848
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-04 08:33:00 +0100

    Addition of account avatar button for the log in flow

--- training.automation.selenium/src/main/java/application/sections/Header.java
+++ training.automation.selenium/src/main/java/application/sections/Header.java
@@ -2,10 +2,12 @@ package application.sections;
 
 import org.openqa.selenium.By;
 
+import application.pages.elements.selenium.Button;
 import application.pages.elements.selenium.Image;
 
 public class Header extends Section
 {
+	public Button accountAvatar;
 	public Image backToHome;
 	public Image trelloLogoHome;
 	
@@ -17,7 +19,8 @@ public class Header extends Section
 	
 	private void buildSections()
 	{
-		backToHome =	new Image(By.xpath("//*[@id=\"header\"]/div[1]/a"), "Home icon in top left", name);
-		trelloLogoHome =new Image(By.xpath("//*[@id=\"header\"]/a/span[2]"), "Trello Logo Home Button", name);
+		accountAvatar	= new Button(By.xpath("//*[@id=\"header\"]/div[5]/a[4]/span"), "Account Avatar button", name);
+		backToHome		= new Image(By.xpath("//*[@id=\"header\"]/div[1]/a"), "Home Icon in Top Left", name);
+		trelloLogoHome	= new Image(By.xpath("//*[@id=\"header\"]/a/span[2]"), "Trello Logo Home Button", name);
 	}
 }
