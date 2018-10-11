e9b9d9be69741ff45e81c33af7db028ce75dd92c
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-11 14:49:51 +0100

    Remove JUnit assertions and use TestHelper assert functions

--- training.automation.selenium/src/main/java/utilities/Helper.java
+++ training.automation.selenium/src/main/java/utilities/Helper.java
@@ -1,40 +1,12 @@
 package utilities;
 
-import org.junit.Assert;
-
 import application.DesktopWebsite;
-import data.TrelloWebData;
 
 public class Helper
 {
-	public static void LogIn(String email, String password)
+	public static void DeleteBoard()
 	{
-		SeleniumDriverHelper.getWebDriver().get("http://www.trello.com/login");
-		
-		DesktopWebsite.logInPage.createAnAccount.assertElementIsDisplayed();
-		
-		DesktopWebsite.logInPage.emailAddress.inputText(TrelloWebData.getUsername());
-		
-		DesktopWebsite.logInPage.password.inputText(TrelloWebData.getPassword());
-		
-		DesktopWebsite.logInPage.logInButton.click();
-		
-		DesktopWebsite.boardsPage.addButton.waitForElementToBeClickable();
-		
-		Assert.assertEquals(SeleniumDriverHelper.getWebDriver().getTitle(), "Boards | Trello");
-	}
-	
-	public static void CreateBoard(String boardName)
-	{
-		DesktopWebsite.boardsPage.addButton.click();
-		
-		DesktopWebsite.boardsPage.createNewBoardButton.click();
-		
-		DesktopWebsite.boardsPage.nameInput.inputText(boardName);
-		
-		DesktopWebsite.boardsPage.backgroundSelectionButton.click();
-		
-		DesktopWebsite.boardsPage.createBoardButton.click();
+		DesktopWebsite.header.backToHome.jsClick();
 		
 		try
 		{
@@ -42,26 +14,7 @@ public class Helper
 		}
 		catch (Exception e)
 		{
-
-		}
-		
-		DesktopWebsite.header.backToHome.jsClick();
-		
-		DesktopWebsite.boardsPage.userBoardButton.assertElementIsDisplayed();
-	}
-	
-	public static void DeleteBoard()
-	{
-		DesktopWebsite.header.backToHome.jsClick();
-		
-		//Allow home page to load before trying to click on anything		
-		try
-		{
-			Thread.sleep(3000);
-		}
-		catch (Exception e)
-		{
-		
+			
 		}
 		
 		DesktopWebsite.boardsPage.userBoardButton.click();
