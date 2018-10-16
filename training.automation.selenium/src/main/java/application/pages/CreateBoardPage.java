9d62e085a7479334752e7cfae926a2caf5361ede
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-10-16 12:01:44 +0100

    For modal that pops up when creating a new board

---
+++ training.automation.selenium/src/main/java/application/pages/CreateBoardPage.java
@@ -0,0 +1,26 @@
+package application.pages;
+
+import org.openqa.selenium.By;
+
+import application.pages.elements.selenium.Button;
+import application.pages.elements.selenium.InputBox;
+
+public class CreateBoardPage extends Page
+{
+    public Button backgroundSelectionButton;
+    public Button createBoardButton;
+    public InputBox nameInput;
+
+    public CreateBoardPage()
+	{
+		super("Create Board Page");
+		buildPage();
+	}
+    
+    private void buildPage()
+    {
+        backgroundSelectionButton = new Button(By.xpath("//*[@id=\"classic\"]/div[4]/div/div/div/form/div/ul/li[2]/button"), "Board Background Selection", name);
+        createBoardButton = new Button(By.xpath("//button[@class=\"primary\"][@type=\"submit\"]"), "Create Board Button", name);
+        nameInput = new InputBox(By.xpath("//input[@placeholder='Add board title']"), "Board Name Input Box", name);
+    }
+}
