aa6b6f3ca6c4023c2cd33c57019d5544d5b0ea32
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-12-04 08:22:07 +0000

    update trellowebdata import

--- training.automation.selenium/src/test/java/stepdefinitions/BoardsPageSteps.java
+++ training.automation.selenium/src/test/java/stepdefinitions/BoardsPageSteps.java
@@ -5,7 +5,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import data.TrelloWebData;
+import trello.TrelloWebData;
 
 import static org.hamcrest.Matchers.is;
 
