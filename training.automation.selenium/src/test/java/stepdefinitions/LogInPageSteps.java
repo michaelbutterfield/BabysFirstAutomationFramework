bd3cc8668088d3bc60f75a83283a45dc63287962
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-12-04 08:22:42 +0000

    update trellowebdata import

--- training.automation.selenium/src/test/java/stepdefinitions/LogInPageSteps.java
+++ training.automation.selenium/src/test/java/stepdefinitions/LogInPageSteps.java
@@ -7,7 +7,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import data.TrelloWebData;
+import trello.TrelloWebData;
 import utilities.SeleniumDriverHelper;
 import utilities.TestHelper;
 
