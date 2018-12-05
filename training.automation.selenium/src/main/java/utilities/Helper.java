1e85fff8b59689910651f4dd343d1d4dc82885b8
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-12-05 09:26:32 +0000

    put log in create board into background for specific feature file

--- training.automation.selenium/src/main/java/utilities/Helper.java
+++ training.automation.selenium/src/main/java/utilities/Helper.java
@@ -1,40 +1,9 @@
 package utilities;
 
-import static org.hamcrest.Matchers.is;
-
 import application.DesktopWebsite;
-import trello.TrelloWebData;
 
 public class Helper
 {
-	public static void LogInCreateBoard()
-	{
-		DesktopWebsite.splashPage.logIn.click();
-		DesktopWebsite.logInPage.createAnAccount.waitForElementToVisible();
-		DesktopWebsite.logInPage.emailAddress.inputText(TrelloWebData.getUsername());
-		DesktopWebsite.logInPage.password.inputText(TrelloWebData.getPassword());
-		DesktopWebsite.logInPage.logInButton.click();
-		DesktopWebsite.header.addButton.waitForElementToBeClickable();
-		TestHelper.assertThat(SeleniumDriverHelper.getWebDriver().getTitle(), is("Boards | Trello"), "Assert that the title of the web page '" + SeleniumDriverHelper.getWebDriver().getTitle() + "'" + " is equal to what is expected: 'Boards | Trello'");
-		DesktopWebsite.header.addButton.click();
-		DesktopWebsite.boardsPage.createNewBoardButton.click();
-		DesktopWebsite.createBoardPage.nameInput.inputText("TestBoard");
-		DesktopWebsite.createBoardPage.backgroundSelectionButton.click();
-		DesktopWebsite.createBoardPage.createBoardButton.click();
-		
-		try
-		{
-			Thread.sleep(2000);
-		}
-		catch (Exception e)
-		{
-
-		}
-		
-		DesktopWebsite.header.backToHome.jsClick();
-		DesktopWebsite.boardsPage.userBoardButton.assertElementIsDisplayed();
-	}
-		
 	public static void DeleteBoard()
 	{
 		DesktopWebsite.header.backToHome.jsClick();
